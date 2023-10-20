1b0b92b6ac7e2595e799461ac4fb504475754b20
alex.curtin <alexhcurtin@gmail.com>
2023-10-20 14:42:58 -0400

    add audio, restyle

--- index.js
+++ index.js
@@ -17,6 +17,98 @@ const TYPES = [
 const STRINGS = ["E", "A", "D", "G", "B", "e"];
 const FRETS = [0, 1, 2, 3, 4, 5];
 const DOTS = [3, 5, 7, 9, 12, 15];
+const ALL_TONES = [
+	[
+		"E2",
+		"F2",
+		"F#2",
+		"G2",
+		"G#2",
+		"A2",
+		"A#2",
+		"B2",
+		"C3",
+		"C#3",
+		"D3",
+		"D#3",
+		"E3",
+	],
+	[
+		"A2",
+		"A#2",
+		"B2",
+		"C3",
+		"C#3",
+		"D3",
+		"D#3",
+		"E3",
+		"F3",
+		"F#3",
+		"G3",
+		"G#3",
+		"A3",
+	],
+	[
+		"D3",
+		"D#3",
+		"E3",
+		"F3",
+		"F#3",
+		"G3",
+		"G#3",
+		"A3",
+		"A#3",
+		"B3",
+		"C4",
+		"C#4",
+		"D4",
+	],
+	[
+		"G3",
+		"G#3",
+		"A3",
+		"A#3",
+		"B3",
+		"C4",
+		"C#4",
+		"D4",
+		"D#4",
+		"E4",
+		"F4",
+		"F#4",
+		"G4",
+	],
+	[
+		"B3",
+		"C4",
+		"C#4",
+		"D4",
+		"D#4",
+		"E4",
+		"F4",
+		"F#4",
+		"G4",
+		"G#4",
+		"A4",
+		"A#4",
+		"B4",
+	],
+	[
+		"E4",
+		"F4",
+		"F#4",
+		"G4",
+		"G#4",
+		"A4",
+		"A#4",
+		"B4",
+		"C5",
+		"C#5",
+		"D5",
+		"D#5",
+		"E5",
+	],
+];
 
 const chartContainer = document.querySelector("#chart-container");
 const chart = document.querySelector("#chart");
@@ -24,6 +116,32 @@ const noteSelect = document.querySelector("#note-select");
 const typeSelect = document.querySelector("#type-select");
 const chordNameContainer = document.querySelector("#chord-name");
 const chordTones = document.querySelector("#chord-tones");
+const playBtn = document.querySelector("#play-btn");
+const speedSlider = document.querySelector("#speed");
+
+let chordSequence;
+let stringsData;
+
+const guitar = new Tone.Sampler({
+	urls: {
+		E2: "open-e2.mp3",
+		A2: "open-a2.mp3",
+		D3: "open-d3.mp3",
+		G3: "open-g3.mp3",
+		B3: "open-b3.mp3",
+		E4: "open-e4.mp3",
+	},
+	baseUrl: "/assets/audio/",
+	onload: () => {
+		console.log("loaded");
+	},
+});
+
+const reverb = new Tone.Reverb({
+	decay: 0.2,
+});
+guitar.connect(reverb);
+reverb.toDestination();
 
 NOTES.forEach((note) => {
 	noteSelect.innerHTML += `<option id=${note}>${note.replace(
@@ -118,6 +236,31 @@ const createBarre = (positions) => {
 	pos.append(barre);
 };
 
+const getChordSequence = (stringsArray) => {
+	const speed = 1 / speedSlider.value;
+	if (chordSequence) {
+		chordSequence.dispose();
+	}
+	const audioTones = [];
+	for (let i = 0; i < stringsArray.length; i++) {
+		if (stringsArray[i] !== "X") {
+			audioTones.push(ALL_TONES[i][stringsArray[i]]);
+		}
+	}
+	chordSequence = new Tone.Sequence(
+		function (time, note) {
+			console.log("in sequence", note);
+			guitar.triggerAttackRelease(note, "1n", time);
+		},
+		audioTones,
+		speed,
+	).start(0);
+	chordSequence.humanize = true;
+	chordSequence.humanizeAmount = 0.5;
+	chordSequence.loop = false;
+	console.log("tones", audioTones);
+};
+
 const getChord = async () => {
 	const note = NOTES[noteSelect.selectedIndex];
 	const type = TYPES[typeSelect.selectedIndex].value;
@@ -127,13 +270,13 @@ const getChord = async () => {
 
 	const { fingering, strings } = chord;
 	const fingeringArray = fingering.split(" ").map((el) => parseInt(el) || el);
-	const stringsArray = strings.split(" ").map((el) => parseInt(el) || el);
-
+	stringsData = strings.split(" ").map((el) => parseInt(el) || el);
+	getChordSequence(stringsData);
 	// createChordInfo(chord);
 	chartContainer.classList.remove("hidden");
 
-	const frets = createFretboard(stringsArray.filter((s) => parseInt(s)));
-	const positions = stringsArray.map((el, i) => ({
+	const frets = createFretboard(stringsData.filter((s) => parseInt(s)));
+	const positions = stringsData.map((el, i) => ({
 		finger: fingeringArray[i],
 		string: STRINGS[i],
 		fret: el,
@@ -162,6 +305,18 @@ const getChord = async () => {
 		}
 	});
 };
+
+speedSlider.addEventListener("change", () => {
+	getChordSequence(stringsData);
+});
+
+playBtn.addEventListener("click", () => {
+	Tone.Transport.stop();
+	if (Tone.context.data !== "running") {
+		Tone.start();
+	}
+	Tone.Transport.start();
+});
 
 noteSelect.addEventListener("change", getChord);
 typeSelect.addEventListener("change", getChord);
